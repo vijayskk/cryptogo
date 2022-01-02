f76beccb00994d41302e0b95f425c529480195ef
vijayskk <vijayskk007@gmail.com>
2022-01-02 09:49:25 +0530

    Graph ok

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -34,65 +34,126 @@ class DetailsScreen extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(
+              height: 10,
+            ),
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width,
-              // child: LineChart(
-              //   LineChartData(
-              //       // read about it in the LineChartData section
-              //       ),
-              //   swapAnimationDuration: Duration(milliseconds: 150), // Optional
-              //   swapAnimationCurve: Curves.linear, // Optional
-              // ),
+              height: MediaQuery.of(context).size.width * 0.5,
+              child: LineChart(
+                LineChartData(
+                  titlesData: FlTitlesData(
+                    leftTitles: SideTitles(
+                      showTitles: true,
+                      getTextStyles: (context, value) {
+                        switch (value.toInt()) {
+                          case 2:
+                            return TextStyle(color: Colors.greenAccent);
+                          case 0:
+                            return TextStyle(color: Colors.white);
+                          case -2:
+                            return TextStyle(color: Colors.redAccent);
+                        }
+                        return TextStyle(color: Colors.white);
+                      },
+                      reservedSize: 50,
+                      margin: 8,
+                      getTitles: (value) {
+                        switch (value.toInt()) {
+                          case 2:
+                            return 'High';
+                          case 0:
+                            return 'Base';
+                          case -2:
+                            return 'Low';
+                        }
+                        return '';
+                      },
+                    ),
+                  ),
+                  minX: 0,
+                  maxX: 12,
+                  minY: -2,
+                  maxY: 2,
+                  lineBarsData: [
+                    LineChartBarData(
+                      // isCurved: true,
+                      spots: [
+                        FlSpot(1, 0),
+                        FlSpot(2, 2),
+                        FlSpot(3, 2),
+                        FlSpot(4, -2),
+                        FlSpot(5, 0),
+                        FlSpot(6, 0),
+                        FlSpot(7, -2),
+                        FlSpot(8, 0),
+                        FlSpot(9, 2),
+                        FlSpot(10, 2),
+                        FlSpot(11, -2),
+                        FlSpot(12, 0),
+                      ],
+                    )
+                  ],
+                ),
+                swapAnimationDuration: Duration(milliseconds: 150),
+                swapAnimationCurve: Curves.linear,
+              ),
+            ),
+            SizedBox(
+              height: 10,
             ),
             ColumnBuilder(
                 itemBuilder: (ctx, index) {
-                    return ListTile(
-                      leading: CircleAvatar(
-                        radius: 20,
-                        backgroundColor: buttonbgcolor,
-                        child: Image.network("https://cryptologos.cc/logos/bitcoin-btc-logo.png",height: 20,width: 20,),
-                      ),
-                      title:                 Text(
-                        "Bitcoin",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        "Live coins",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      trailing: Column(
-                        children: [
-                                              Padding(
-                      padding: const EdgeInsets.only(bottom: 2),
-                      child: Text(
-                        "16.8B",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold),
+                  return ListTile(
+                    leading: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: buttonbgcolor,
+                      child: Image.network(
+                        "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+                        height: 20,
+                        width: 20,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 2),
-                      child: Text(
-                        "7.98%",
-                        style: TextStyle(
-                            color: greentxtcolor,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                      ),
+                    title: Text(
+                      "Bitcoin",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
                     ),
-                        ],
-                      ),
-                    );
-                  },
+                    subtitle: Text(
+                      "Live coins",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 2),
+                          child: Text(
+                            "16.8B",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 2),
+                          child: Text(
+                            "7.98%",
+                            style: TextStyle(
+                                color: greentxtcolor,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
